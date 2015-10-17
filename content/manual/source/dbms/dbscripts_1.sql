bb604ef06a9bb5d2be7aa99c8ac364f4c4474148
krivopustov <krivopustov@haulmont.com>
2015-10-17 16:50:21 +0400

    Changes for platform 6.0 (dbms). #PLD-110

--- content/manual/source/dbms/dbscripts_1.sql
+++ content/manual/source/dbms/dbscripts_1.sql
@@ -1,10 +1,8 @@
 create table SALES_CUSTOMER (
-  ID varchar(36) not null ,
+  ID varchar(36) not null,
   CREATE_TS timestamp,
   CREATED_BY varchar(50),
-  --
   NAME varchar(100),
   ADDRESS varchar(200), -- added column
-  --
   primary key (ID)
 )
