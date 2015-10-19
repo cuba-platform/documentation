008d00318dbc85154e7a7efa355fad032ba134e6
Konstantin Krivopustov <krivopustov@haulmont.com>
2015-10-19 11:31:05 +0400

    Changes for platform 6.0 (middleware). #PLD-110

--- content/manual/source/middleware/transactions_2.java
+++ content/manual/source/middleware/transactions_2.java
@@ -1,5 +1,9 @@
-persistence.createTransaction().execute(new Transaction.Runnable() {
-    public void run(EntityManager em) {
-        // transactional code here
-    }
+UUID customerId = persistence.createTransaction().execute((EntityManager em) -> {
+    Customer customer = metadata.create(Customer.class);
+    customer.setName("ABC");
+    em.persist(customer);
+    return customer.getId();
 });
+
+Customer customer = persistence.createTransaction().execute(em ->
+        em.find(Customer.class, customerId, "_local"));
