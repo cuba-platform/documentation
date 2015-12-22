8f6f40f366edb72f569b5fdca4aacd29e087a60f
krivopustov <krivopustov@haulmont.com>
2015-12-22 18:36:31 +0400

    Describe DTYPE in "Extending an Entity" section. #PLD-132

---
+++ content/manual/source/extension/entity_extension_1.java
@@ -0,0 +1,15 @@
+@Entity(name = "sales$ExtUser")
+@Extends(User.class)
+public class ExtUser extends User {
+
+    @Column(name = "ADDRESS", length = 100)
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ content/manual/source/extension/entity_extension_2.sql
@@ -0,0 +1,9 @@
+-- add column for "address" attribute
+alter table SEC_USER add column ADDRESS varchar(100)
+^
+-- add discriminator column required for entity inheritance
+alter table SEC_USER add column DTYPE varchar(100)
+^
+-- set discriminator value for existing records
+update SEC_USER set DTYPE = 'sales$ExtUser' where DTYPE is null
+^
