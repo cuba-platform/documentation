f4ed8dac1cf11acd8ee97551783944092f5e7aa9
krivopustov <krivopustov@haulmont.com>
2015-12-22 12:47:54 +0400

    EntityManager example

---
+++ content/manual/source/middleware/entityManager_1.java
@@ -0,0 +1,24 @@
+@Service(SalesService.NAME)
+public class SalesServiceBean implements SalesService {
+
+    @Inject
+    private Persistence persistence;
+
+    @Override
+    public BigDecimal calculateSales(Object customerId) {
+        BigDecimal result;
+        // start transaction
+        try (Transaction tx = persistence.createTransaction()) {
+            // get EntityManager for the current transaction
+            EntityManager em = persistence.getEntityManager();
+            // create and execute Query
+            Query query = em.createQuery(
+                    "select sum(o.amount) from sample$Order o where o.customer.id = :customerId");
+            query.setParameter("customerId", customerId);
+            result = (BigDecimal) query.getFirstResult();
+            // commit transaction
+            tx.commit();
+        }
+        return result != null ? result : BigDecimal.ZERO;
+    }
+}
