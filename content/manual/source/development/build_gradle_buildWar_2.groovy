dceb82d81de6e8e1085e1906b53781efa62ec1e3
Konstantin Krivopustov <krivopustov@haulmont.com>
2016-01-27 17:59:52 +0400

    Fix WAR deployment description

--- content/manual/source/development/build_gradle_buildWar_2.groovy
+++ content/manual/source/development/build_gradle_buildWar_2.groovy
@@ -1,7 +1,4 @@
 task buildWar(type: CubaWarBuilding) {
-    coreProject = project(':app-core')
-    webProject = project(':app-web')
-    appName = 'app'
     appHome = '..'
     appProperties = ['cuba.automaticDatabaseUpdate': 'true']
     singleWar = false
