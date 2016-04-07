0c868b089e69c174c97bf0b51f5272447a08ed00
Konstantin Krivopustov <krivopustov@haulmont.com>
2016-04-07 16:32:57 +0400

    PLD-180 Custom style for the desktop application

--- content/manual/source/gui/themes_14.java
+++ content/manual/source/gui/themes_14.java
@@ -1,14 +1,13 @@
-public class UnderlinedLabelDecorator implements ComponentDecorator {
-
+public class UnderlinedButtonDecorator implements ComponentDecorator {
     @Override
     @SuppressWarnings("unchecked")
     public void decorate(Object component, Set<String> state) {
-        DesktopLabel item = (DesktopLabel) component;
-        JLabel jlabel = item.getComponent();
-
-        Font originalFont = jlabel.getFont();
+        DesktopButton item = (DesktopButton) component;
+        JButton jButton = (JButton) item.getComponent();
+        Font originalFont = jButton.getFont();
         Map attributes = originalFont.getAttributes();
+
         attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
-        jlabel.setFont(originalFont.deriveFont(attributes));
+        jButton.setFont(originalFont.deriveFont(attributes));
     }
 }
