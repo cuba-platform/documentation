c7c37a48da0acae2c8d93c2d56a1156392093c2d
Konstantin Krivopustov <krivopustov@haulmont.com>
2016-06-07 10:35:54 +0400

    PLD-236 Incorrect path to single-war-web.xml in buildWar example

--- content/manual/source/development/build_gradle_buildWar_1.groovy
+++ content/manual/source/development/build_gradle_buildWar_1.groovy
@@ -1,4 +1,4 @@
 task buildWar(type: CubaWarBuilding) {
     appHome = '..'
-    webXmlPath = 'modules/web/WEB-INF/single-war-web.xml'
+    webXmlPath = 'modules/web/web/WEB-INF/single-war-web.xml'
 }
