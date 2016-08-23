fb72ef3cd235b6584971c44be05dc70ad81c4188
Nikita Petunin <petunin@haulmont.com>
2016-08-23 11:18:32 +0400

    PLD-276 Custom JSON for Chart in XML

---
+++ content/charts/source/chart/custom_json_2.java
@@ -0,0 +1,17 @@
+@Inject
+protected Chart serialChart;
+
+@Override
+public void init(Map<String, Object> params) {
+    super.init(params);
+
+    ListDataProvider serialChartDataProvider = new ListDataProvider();
+    int[] serialChartChartData = {5, 7, 6, 9, 7, 8, 5, 6, 4, 6, 5, 7, 4, 5, 3, 4, 2, 0};
+
+    for (int i = 0; i < redLineChartData.length; i++) {
+        serialChartDataProvider.addItem(graphData(serialChartChartData[i]));
+    }
+
+    SerialChart serialChartConfiguration = (SerialChart) serialChart.getConfiguration();
+    serialChartConfiguration.setDataProvider(serialChartDataProvider);
+}
