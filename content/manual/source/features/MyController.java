2b2e6fed85e990d86fb01af3cee7d146e38d798f
Maxim Gorbunkov <gorbunkov@haulmont.com>
2016-10-03 19:19:29 +0400

    PLD-305 Custom OAuth2 protected REST controllers creation

--- content/manual/source/features/MyController.java
+++ content/manual/source/features/MyController.java
@@ -3,13 +3,17 @@ package com.company.test.portal.myapi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.company.test.services.SomeService;
 
 @RestController
 @RequestMapping("/myapi")
 public class MyController {
 
+    @Inject
+    protected SomeService someService;
+
     @GetMapping("/dosmth")
     public String doSmth() {
-        return "done";
+        return someService.getResult();
     }
 }
