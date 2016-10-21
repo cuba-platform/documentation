93e976c0a2f29b1540ff14291d3c55072d1febe4
Olga Shiryaeva <shiryaeva@haulmont.com>
2016-10-21 14:53:38 +0400

    PLD-222 Document SourceCodeEditor + platform-doc/review/PD-CR-28

--- content/manual/source/gui_vcl/sourceCodeEditor_2.java
+++ content/manual/source/gui_vcl/sourceCodeEditor_2.java
@@ -12,25 +12,13 @@ private CheckBox showGutterCheck;
 @Override
 public void init(Map<String, Object> params) {
     highlightActiveLineCheck.setValue(simpleCodeEditor.isHighlightActiveLine());
-    highlightActiveLineCheck.addValueChangeListener(e -> {
-        if (e.getValue() != null) {
-            simpleCodeEditor.setHighlightActiveLine((Boolean) e.getValue());
-        }
-    });
+    highlightActiveLineCheck.addValueChangeListener(e -> simpleCodeEditor.setHighlightActiveLine(Boolean.TRUE.equals(e.getValue())));
 
     printMarginCheck.setValue(simpleCodeEditor.isShowPrintMargin());
-    printMarginCheck.addValueChangeListener(e -> {
-        if (e.getValue() != null) {
-            simpleCodeEditor.setShowPrintMargin((Boolean) e.getValue());
-        }
-    });
+    printMarginCheck.addValueChangeListener(e -> simpleCodeEditor.setShowPrintMargin(Boolean.TRUE.equals(e.getValue())));
 
     showGutterCheck.setValue(simpleCodeEditor.isShowGutter());
-    showGutterCheck.addValueChangeListener(e -> {
-        if (e.getValue() != null) {
-            simpleCodeEditor.setShowGutter((Boolean) e.getValue());
-        }
-    });
+    showGutterCheck.addValueChangeListener(e -> simpleCodeEditor.setShowGutter(Boolean.TRUE.equals(e.getValue())));
 
     Map<String, Object> modes = new HashMap<>();
     for (SourceCodeEditor.Mode mode : SourceCodeEditor.Mode.values()) {
@@ -39,9 +27,5 @@ public void init(Map<String, Object> params) {
 
     modeField.setOptionsMap(modes);
     modeField.setValue(SourceCodeEditor.Mode.Text);
-    modeField.addValueChangeListener(e -> {
-        if (e.getValue() != null) {
-            simpleCodeEditor.setMode((SourceCodeEditor.Mode) e.getValue());
-        }
-    });
+    modeField.addValueChangeListener(e -> simpleCodeEditor.setMode((SourceCodeEditor.Mode) e.getValue()));
 }
