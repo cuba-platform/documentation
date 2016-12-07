3b1c5a91089c191bed2bf008acd027abdb828c8f
Olga Shiryaeva <shiryaeva@haulmont.com>
2016-12-07 16:22:37 +0400

    PLD-218 Description how to add custom icons to project: updated App.java file issue

--- content/manual/source/gui/add_icons_App.java
+++ content/manual/source/gui/add_icons_App.java
@@ -1,3 +1,6 @@
+import com.haulmont.cuba.web.DefaultApp;
+import com.haulmont.cuba.web.gui.components.WebComponentsHelper;
+
 public class App extends DefaultApp {
     static {
         WebComponentsHelper.registerFontIcon("ico-moon-icon", IcoMoon.class);
