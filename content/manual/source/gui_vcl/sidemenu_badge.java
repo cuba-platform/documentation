938060f13394589499be7938815721cd3cdcee1f
Olga Shiryaeva <shiryaeva@haulmont.com>
2017-01-31 16:46:16 +0400

    Update sidemenu_badge.java

--- content/manual/source/gui_vcl/sidemenu_badge.java
+++ content/manual/source/gui_vcl/sidemenu_badge.java
@@ -1,6 +1,6 @@
 int count = 5;
 SideMenu.MenuItem item = sideMenu.createMenuItem("count");
 item.setCaption("Messages");
-item.setBadgeText(count+" new");
+item.setBadgeText(count + " new");
 item.setIcon("font-icon:ENVELOPE");
 sideMenu.addMenuItem(item,0);
