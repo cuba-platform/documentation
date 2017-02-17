6e80da09660378b9a0eee7cdde6120c003901a1b
Olga Shiryaeva <shiryaeva@haulmont.com>
2017-02-17 10:09:04 +0400

    PLD-379 Update documentation on componentsFactory.createComponent

--- content/manual/source/gui_vcl/fieldgroup_3.java
+++ content/manual/source/gui_vcl/fieldgroup_3.java
@@ -8,7 +8,7 @@ public void init(Map<String, Object> params) {
     fieldGroup.addCustomField("password", new FieldGroup.CustomFieldGenerator() {
         @Override
         public Component generateField(Datasource datasource, String propertyId) {
-            PasswordField passwordField = componentsFactory.createComponent(PasswordField.NAME);
+            PasswordField passwordField = componentsFactory.createComponent(PasswordField.class);
             passwordField.setDatasource(datasource, propertyId);
             return passwordField;
         }
