c36db553211c516650364dab5377d76114175809
Konstantin Krivopustov <krivopustov@haulmont.com>
2017-05-19 19:28:06 +0400

    PLD-481 dataManager.load(loadContext) code example incorrect

--- content/manual/source/common/dataManager_1.java
+++ content/manual/source/common/dataManager_1.java
@@ -2,13 +2,13 @@
 private DataManager dataManager;
 
 private Book loadBookById(UUID bookId) {
-    LoadContext loadContext = LoadContext.create(Book.class)
+    LoadContext<Book> loadContext = LoadContext.create(Book.class)
             .setId(bookId).setView("book.edit");
     return dataManager.load(loadContext);
 }
 
 private List<BookPublication> loadBookPublications(UUID bookId) {
-    LoadContext loadContext = LoadContext.create(BookPublication.class)
+    LoadContext<BookPublication> loadContext = LoadContext.create(BookPublication.class)
             .setQuery(LoadContext.createQuery("select p from library$BookPublication p where p.book.id = :bookId")
                 .setParameter("bookId", bookId))
             .setView("bookPublication.full");
