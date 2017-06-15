4dfc40404fe2ffe0c8c229a4b54cbefc39571ed8
Konstantin Krivopustov <krivopustov@haulmont.com>
2017-06-15 17:12:29 +0400

    PLD-357 Custom web.xml for separate WARs in CubaWarBuilding task

---
+++ content/manual/source/development/build_gradle_buildWar_4.groovy
@@ -0,0 +1,6 @@
+task buildWar(type: CubaWarBuilding) {
+    singleWar = false
+    // ...
+    coreWebXmlPath = 'modules/core/web/WEB-INF/production-web.xml'
+    webWebXmlPath = 'modules/web/web/WEB-INF/production-web.xml'
+}
