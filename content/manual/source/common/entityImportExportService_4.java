e77a7722a17e4d6838336ff3526d8d1c20224177
Konstantin Krivopustov <krivopustov@haulmont.com>
2017-07-06 11:14:09 +0400

    PLD-202 Describe EntityImportExportService (moved to features)

--- content/manual/source/common/entityImportExportService_4.java
+++
@@ -1,9 +0,0 @@
-@Inject
-private EntityImportExportService entityImportExportService;
-@Inject
-private GroupDatasource<Customer, UUID> customersDs;
-
-...
-EntityImportView view = new EntityImportView(Customer.class);
-view.addLocalProperties();
-Collection<Entity> collection = entityImportExportService.importEntities(customersDs.getItems(), view);
