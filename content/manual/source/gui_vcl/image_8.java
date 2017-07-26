1f7e77a6e0450fb19886c4fd4948adb846a95cac
Olga Shiryaeva <shiryaeva@haulmont.com>
2017-07-26 16:24:47 +0400

    cosmetics

--- content/manual/source/gui_vcl/image_8.java
+++ content/manual/source/gui_vcl/image_8.java
@@ -1,6 +1,6 @@
 frameworksTable.addGeneratedColumn("image", entity -> {
     Image image = componentsFactory.createComponent(Image.class);
     image.setDatasource(frameworksTable.getItemDatasource(entity), "image");
-    image.setHeight("100");
+    image.setHeight("100px");
     return image;
 });
