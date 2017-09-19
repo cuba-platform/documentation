2885a155e9dcf67253101336f21c04ec18b56dda
Olga Shiryaeva <shiryaeva@haulmont.com>
2017-09-19 17:35:10 +0400

    PLD-609 Improve documentation for imperative FieldGroup field creating

--- content/manual/source/gui_vcl/fieldgroup_3.java
+++ content/manual/source/gui_vcl/fieldgroup_3.java
@@ -2,9 +2,12 @@
 protected FieldGroup fieldGroup;
 @Inject
 protected ComponentsFactory componentsFactory;
+@Inject
+private Datasource<User> userDs;
 
 @Override
 public void init(Map<String, Object> params) {
         PasswordField passwordField = componentsFactory.createComponent(PasswordField.class);
+        passwordField.setDatasource(userDs, "password");
         fieldGroup.getFieldNN("password").setComponent(passwordField);
 }
