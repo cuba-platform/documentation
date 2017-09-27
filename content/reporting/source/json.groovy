a456c2170e1ef2be15a3f62f4d0eb6c2791ac8ae
Olga Shiryaeva <shiryaeva@haulmont.com>
2017-09-27 14:51:14 +0400

    PLD-608 Add JSON dataset type to Reports

---
+++ content/reporting/source/json.groovy
@@ -0,0 +1,16 @@
+return '{\n' +
+        '  "items": [\n' +
+        '    {\n' +
+        '      "name": "Java Concurrency in practice",\n' +
+        '      "price": 15000\n' +
+        '    },\n' +
+        '    {\n' +
+        '      "name": "Clear code",\n' +
+        '      "price": 13000\n' +
+        '    },\n' +
+        '    {\n' +
+        '      "name": "Scala in action",\n' +
+        '      "price": 12000\n' +
+        '    }\n' +
+        '  ]\n' +
+        '} '
