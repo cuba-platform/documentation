8026eb84c4370c9707ade711edd56664222ac986
Olga Shiryaeva <shiryaeva@haulmont.com>
2017-10-04 15:28:40 +0400

    multiline JSON string

--- content/reporting/source/json.groovy
+++ content/reporting/source/json.groovy
@@ -1,16 +1,18 @@
-return '{\n' +
-        '  "items": [\n' +
-        '    {\n' +
-        '      "name": "Java Concurrency in practice",\n' +
-        '      "price": 15000\n' +
-        '    },\n' +
-        '    {\n' +
-        '      "name": "Clear code",\n' +
-        '      "price": 13000\n' +
-        '    },\n' +
-        '    {\n' +
-        '      "name": "Scala in action",\n' +
-        '      "price": 12000\n' +
-        '    }\n' +
-        '  ]\n' +
-        '} '
+return ''' 
+        {
+          "items": [
+            {
+              "name": "Java Concurrency in practice",
+              "price": 15000
+            },
+            {
+              "name": "Clear code",
+              "price": 13000
+            },
+            {
+              "name": "Scala in action",
+              "price": 12000
+            }
+          ]
+        }
+        '''
