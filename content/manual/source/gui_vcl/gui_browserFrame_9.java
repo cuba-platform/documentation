488a05dc84675c4ed6352fed4ae913c2aef0fe94
Olga Shiryaeva <shiryaeva@haulmont.com>
2017-10-05 11:01:22 +0400

    Re: platform-doc | 6.7 Pld 560 document the browser frame component (!97)

--- content/manual/source/gui_vcl/gui_browserFrame_9.java
+++ content/manual/source/gui_vcl/gui_browserFrame_9.java
@@ -2,5 +2,5 @@ BrowserFrame frame = componentsFactory.createComponent(BrowserFrame.class);
 try {
     frame.setSource(UrlResource.class).setUrl(new URL("http://www.foobar2000.org/"));
 } catch (MalformedURLException e) {
-    throw new RuntimeException(e.getLocalizedMessage());
+    throw new RuntimeException(e);
 }
