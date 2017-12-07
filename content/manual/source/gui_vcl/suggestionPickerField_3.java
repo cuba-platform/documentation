f7d3a2eb1741b9bb54025b59a511be3270bb0d3b
Olga Shiryaeva <shiryaeva@haulmont.com>
2017-12-07 14:35:09 +0400

    PLD-695 In the example of the setSearchExecutor usage change searchParams parameter to the searchString parameter

--- content/manual/source/gui_vcl/suggestionPickerField_3.java
+++ content/manual/source/gui_vcl/suggestionPickerField_3.java
@@ -1,4 +1,4 @@
 suggestionPickerField.setSearchExecutor((searchString, searchParams) ->
         dataManager.loadList(LoadContext.create(Customer.class).setQuery(
             LoadContext.createQuery("select c from sample$Customer c where c.name like :name order by c.name")
-                .setParameter("name", "%" + searchParams + "%"))));
+                .setParameter("name", "%" + searchString + "%"))));
