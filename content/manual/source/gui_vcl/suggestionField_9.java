9e65f69b61aa9ab6f980c7d65efd27ff035ea971
Olga Shiryaeva <shiryaeva@haulmont.com>
2018-01-19 09:13:43 +0400

    PLD-736 Update SuggestionPickerField setSearchExecutor example with escapeForLike

--- content/manual/source/gui_vcl/suggestionField_9.java
+++ content/manual/source/gui_vcl/suggestionField_9.java
@@ -1,4 +1,4 @@
-suggestionPickerField.setSearchExecutor((searchString, searchParams) -> {
+suggestionField.setSearchExecutor((searchString, searchParams) -> {
     searchString = QueryUtils.escapeForLike(searchString);
     return dataManager.loadList(LoadContext.create(Customer.class).setQuery(
             LoadContext.createQuery("select c from sample$Customer c where c.name like :name order by c.name escape '\\'")
