1d12a32ae553204d7efe14a2ab5b8047cbfa89aa
Olga Shiryaeva <shiryaeva@haulmont.com>
2018-01-19 09:08:02 +0400

    PLD-736 Update SuggestionPickerField setSearchExecutor example with escapeForLike

--- content/manual/source/gui_vcl/suggestionPickerField_3.java
+++ content/manual/source/gui_vcl/suggestionPickerField_3.java
@@ -1,16 +1,6 @@
-public class OrderEdit extends AbstractEditor<Order> {
-
-    @Inject
-    private SuggestionPickerField suggestionPickerField;
-
-    @Inject
-    private DataManager dataManager;
-
-    @Override
-    public void init(Map<String, Object> params) {
-        suggestionPickerField.setSearchExecutor((searchString, searchParams) ->
-                dataManager.loadList(LoadContext.create(Customer.class).setQuery(
-                    LoadContext.createQuery("select c from sample$Customer c where c.name like :name order by c.name")
-                        .setParameter("name", "%" + searchString + "%"))));
-    }
-}
+suggestionPickerField.setSearchExecutor((searchString, searchParams) -> {
+    searchString = QueryUtils.escapeForLike(searchString);
+    return dataManager.loadList(LoadContext.create(Customer.class).setQuery(
+            LoadContext.createQuery("select c from sample$Customer c where c.name like :name order by c.name escape '\\'")
+                    .setParameter("name", "%" + searchString + "%")));
+});
