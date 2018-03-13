569753cbf2a757c1362d7cfb56c71b3d013d01f5
Olga Shiryaeva <shiryaeva@haulmont.com>
2018-03-13 10:19:20 +0400

    Fix integration test examples #40
    (cherry picked from commit 27a6525)

--- content/manual/source/development/testing_3.java
+++ content/manual/source/development/testing_3.java
@@ -2,7 +2,7 @@ public class SalesTestContainer extends TestContainer {
 
     public SalesTestContainer() {
         super();
-        appComponents = new ArrayList<>(Arrays.asList(
+        appComponents = Arrays.asList(
                 "com.haulmont.cuba"
                 // add CUBA premium add-ons here
                 // "com.haulmont.bpm",
@@ -10,15 +10,16 @@ public class SalesTestContainer extends TestContainer {
                 // "com.haulmont.fts",
                 // "com.haulmont.reports",
                 // and custom app components if any
-        ));
+        );
         appPropertiesFiles = Arrays.asList(
                 // List the files defined in your web.xml
                 // in appPropertiesConfig context parameter of the core module
-                "sales-app.properties",
+                "com/company/sales/app.properties",
                 // Add this file which is located in CUBA and defines some properties
                 // specifically for test environment. You can replace it with your own
                 // or add another one in the end.
-                "test-app.properties");
+                "test-app.properties",
+                "com/company/sales/test-app.properties");
         dbDriver = "org.postgresql.Driver";
         dbUrl = "jdbc:postgresql://localhost/sales_test";
         dbUser = "cuba";
