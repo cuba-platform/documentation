211f79ba148757661fa8e2328156c175b547f844
Konstantin Krivopustov <krivopustov@haulmont.com>
2018-03-15 10:28:07 +0400

    Simplify test-app.properties example #40

--- content/manual/source/development/testing_11.java
+++ content/manual/source/development/testing_11.java
@@ -1,7 +1,2 @@
 cuba.webContextName = app-core
-cuba.availableLocales=English (GB)|en;English (US)|en_US;French|fr;French (CA)|fr_CA
-cuba.trustedClientPermittedIpList=127.0.0.1, 10.17.*.*
-cuba.confDir=${user.dir}/test-home/cuba/conf
-cuba.logDir=${user.dir}/test-home/cuba/logs
-cuba.tempDir=${user.dir}/test-home/cuba/temp
-cuba.dataDir=${user.dir}/test-home/cuba/work
+sales.someProperty = someValue
