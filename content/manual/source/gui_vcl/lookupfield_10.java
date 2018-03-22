7665e285e9620c09c1b020908c1e673d9ad93726
Olga Shiryaeva <shiryaeva@haulmont.com>
2018-03-22 18:01:08 +0400

    New property of the WebJarResource annotation #79

--- content/manual/source/gui_vcl/lookupfield_10.java
+++
@@ -1,9 +0,0 @@
-lookupField.setOptionIconProvider(item -> {
-    RoleType roleType = (RoleType) item;
-
-    if (roleType.ordinal() % 2 == 0) {
-        return "icons/alarm.svg";
-    }
-
-    return "icons/cloud.svg";
-});
