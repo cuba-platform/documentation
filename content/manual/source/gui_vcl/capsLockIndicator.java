7b295ffc0688059886b1b8d74b6e6b3c054126d8
shiryaeva <shiryaeva@haulmont.com>
2018-04-06 14:01:40 +0400

    CapslockIndicator component #107 cosmetics in code snippets

--- content/manual/source/gui_vcl/capsLockIndicator.java
+++ content/manual/source/gui_vcl/capsLockIndicator.java
@@ -1,5 +1,3 @@
-CapsLockIndicator indicator = componentsFactory.createComponent(CapsLockIndicator.class);
-indicator.setId("indicator");
-indicator.setCapsLockOnMessage("Caps Lock is ON");
-indicator.setCapsLockOffMessage("Please enter the password");
-passwordField.setCapsLockIndicator(indicator);
+CapsLockIndicator capsLockIndicator = componentsFactory.createComponent(CapsLockIndicator.class);
+capsLockIndicator.setId("capsLockIndicator");
+passwordField.setCapsLockIndicator(capsLockIndicator);
