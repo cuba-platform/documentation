678be7b04cee243eb58f5249453b2eb9249b6edb
Ronald <43592048+crontabpy@users.noreply.github.com>
2018-09-27 19:57:10 +0400

    set datasource line code as recommended (#224)

--- content/manual/source/gui/datasources_1.java
+++ content/manual/source/gui/datasources_1.java
@@ -12,8 +12,8 @@ public void init(Map<String, Object> params){
     // Get customer from component: not for common use
     Component component = fieldGroup.getFieldNN("customer").getComponentNN();
     customer = ((HasValue)component).getValue();
-    // Get customer from component: recommended
+    // Get customer from component
     customer = customerField.getValue();
-    // Get customer from datasource
+    // Get customer from datasource: recommended
     customer = orderDs.getItem().getCustomer();
 }
