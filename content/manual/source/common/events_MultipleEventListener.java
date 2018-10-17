b1cb74600583e5504a7048812e0bc174b0705a3f
Olga Shiryaeva <shiryaeva@haulmont.com>
2018-10-17 17:07:02 +0400

    Change @EventListener method modifier to protected in examples #241

--- content/manual/source/common/events_MultipleEventListener.java
+++ content/manual/source/common/events_MultipleEventListener.java
@@ -2,13 +2,13 @@
 public class MultipleEventListener {
     @Order(10)
     @EventListener
-    private void handleDemoEvent(DemoEvent event) {
+    protected void handleDemoEvent(DemoEvent event) {
         // handle event
     }
 
     @Order(1010)
     @EventListener
-    private void handleUserLoginEvent(UserLoggedInEvent event) {
+    protected void handleUserLoginEvent(UserLoggedInEvent event) {
         // handle event
     }
 }
