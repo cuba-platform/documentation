819257898d2b1af08860a75f7368fe122d1963ba
Konstantin Krivopustov <krivopustov@haulmont.com>
2018-11-28 11:44:56 +0400

    Fix ApplicationEvent examples

--- content/manual/source/common/events_DemoBean.java
+++ content/manual/source/common/events_DemoBean.java
@@ -1,5 +1,14 @@
+package com.company.sales.core;
+
+import com.haulmont.cuba.core.global.Events;
+import com.haulmont.cuba.core.global.UserSessionSource;
+import com.haulmont.cuba.security.global.UserSession;
+import org.springframework.stereotype.Component;
+import javax.inject.Inject;
+
 @Component
 public class DemoBean {
+
     @Inject
     private Events events;
     @Inject
@@ -7,6 +16,6 @@ public class DemoBean {
 
     public void demo() {
         UserSession userSession = userSessionSource.getUserSession();
-        events.publish(new DemoEvent(userSession.getUser()));
+        events.publish(new DemoEvent(this, userSession.getUser()));
     }
 }
