430567808b8c4dfcb91edc8b2882bd5f1b4f98e6
shiryaeva <shiryaeva@haulmont.com>
2018-12-18 10:06:06 +0400

    Document JavaScriptComponent (updated examples) #279

--- content/manual/source/gui/js_component/jsComponent_1.java
+++ content/manual/source/gui/js_component/jsComponent_1.java
@@ -1,6 +1,4 @@
 jsComponent.addDependencies(
-        "webjar://jquery:jquery.min.js",
-        "webjar://jquery-ui:jquery-ui.min.js",
-        "webjar://jquery-ui:jquery-ui.css",
-        "vaadin://slider-connector.js"
+        "webjar://leaflet.js",
+        "webjar://leaflet-draw.js"
 );
