7d57d4e0dfedd48ff6c36345b68b115cc5683faf
Konstantin Krivopustov <krivopustov@haulmont.com>
2018-12-27 11:12:20 +0400

    Refactor cookbook

--- content/manual/source/middleware/entitylisteners_1.java
+++ content/manual/source/middleware/entitylisteners_1.java
@@ -1,22 +1,49 @@
-@Component("sample_MyEntityListener")
-public class MyEntityListener implements
-        BeforeInsertEntityListener<MyEntity>,
-        BeforeUpdateEntityListener<MyEntity> {
+package com.company.sample.listener;
+
+import com.company.sample.core.DiscountCalculator;
+import com.company.sample.entity.*;
+import com.haulmont.cuba.core.EntityManager;
+import com.haulmont.cuba.core.listener.*;
+import org.springframework.stereotype.Component;
+import javax.inject.Inject;
+import java.math.BigDecimal;
+
+@Component("sample_OrderEntityListener")
+public class OrderEntityListener implements
+        BeforeInsertEntityListener<Order>,
+        BeforeUpdateEntityListener<Order>,
+        BeforeDeleteEntityListener<Order> {
 
     @Inject
-    protected Metadata metadata;
+    private DiscountCalculator discountCalculator; // a managed bean of the middle tier
 
     @Override
-    public void onBeforeInsert(MyEntity entity, EntityManager entityManager) {
-        Foo foo = metadata.create(Foo.class);
-        ...
-        entity.setFoo(foo);
-        entityManager.persist(foo);
+    public void onBeforeInsert(Order entity, EntityManager entityManager) {
+        calculateDiscount(entity.getCustomer(), entityManager);
     }
 
     @Override
-    public void onBeforeUpdate(MyEntity entity, EntityManager entityManager) {
-        Foo foo = entityManager.find(Foo.class, entity.getFoo().getId());
-        ...
+    public void onBeforeUpdate(Order entity, EntityManager entityManager) {
+        calculateDiscount(entity.getCustomer(), entityManager);
+    }
+
+    @Override
+    public void onBeforeDelete(Order entity, EntityManager entityManager) {
+        calculateDiscount(entity.getCustomer(), entityManager);
+    }
+
+    private void calculateDiscount(Customer customer, EntityManager entityManager) {
+        if (customer == null)
+            return;
+
+        // Delegate calculation to a managed bean of the middle tier
+        BigDecimal discount = discountCalculator.calculateDiscount(customer.getId());
+
+        // Merge customer instance because it comes to onBeforeInsert as part of another
+        // entity's object graph and can be detached
+        Customer managedCustomer = entityManager.merge(customer);
+
+        // Set the discount for the customer. It will be saved on transaction commit.
+        managedCustomer.setDiscount(discount);
     }
 }
