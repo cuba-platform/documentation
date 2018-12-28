b94892ab9c1a5323c0c6ab3edfd617ce6ada08cd
Konstantin Krivopustov <krivopustov@haulmont.com>
2018-12-28 16:11:59 +0400

    Screen events

--- content/manual/source/gui/screens/screen_BeforeCloseEvent_origin.java
+++
@@ -1,5 +0,0 @@
-@Subscribe(target = Target.FRAME)
-protected void onBeforeClose1(Window.BeforeCloseEvent event) {
-    if(event.getCloseOrigin() == CloseOriginType.BREADCRUMBS)
-        event.preventWindowClose();
-}
--- content/manual/source/gui/screens/screen_applySettings.java
+++
@@ -1,8 +0,0 @@
-@Override
-public void applySettings(Settings settings) {
-    super.applySettings(settings);
-    Element xmlDescriptor = settings.get(showPanel.getId());
-    if (xmlDescriptor.attribute("showPanel") != null) {
-        showPanel.setValue(Boolean.parseBoolean(xmlDescriptor.attributeValue("showPanel")));
-    }
-}
