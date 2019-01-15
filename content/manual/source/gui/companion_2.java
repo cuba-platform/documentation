775135871afaf8a60ae9263bcc70b1eba6f14909
Konstantin Krivopustov <krivopustov@haulmont.com>
2019-01-15 19:10:54 +0400

    Fix GUI section

--- content/manual/source/gui/companion_2.java
+++
@@ -1,7 +0,0 @@
-public class WebCustomerBrowseCompanion implements CustomerBrowse.Companion {
-    @Override
-    public void initTable(Table<Customer> table) {
-        com.vaadin.ui.Table webTable = (com.vaadin.ui.Table) WebComponentsHelper.unwrap(table);
-        // do something specific to Vaadin table
-    }
-}
--- content/manual/source/gui/companion_3.java
+++
@@ -1,7 +0,0 @@
-public class DesktopCustomerBrowseCompanion implements CustomerBrowse.Companion {
-    @Override
-    public void initTable(Table<Customer> table) {
-        javax.swing.JTable desktopTable = (javax.swing.JTable) DesktopComponentsHelper.unwrap(table);
-        // do something specific to Swing table
-    }
-}
