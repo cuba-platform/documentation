1264e7913c933bb8e5fcb128cd1d68f038f933e3
Konstantin Krivopustov <krivopustov@haulmont.com>
2019-02-05 18:03:10 +0400

    Mention that DataManager.loadValues() requires data store name if it is not the main store #230

--- content/manual/source/common/dataManager_5.java
+++ content/manual/source/common/dataManager_5.java
@@ -1,6 +1,7 @@
 List<KeyValueEntity> list = dataManager.loadValues(
         "select o.customer, sum(o.amount) from demo$Order o " +
         "where o.date >= :date group by o.customer")
-    .properties("customer", "sum")
+    .store("legacy_db") // <1>
+    .properties("customer", "sum") // <2>
     .parameter("date", orderDate)
     .list();
--- content/manual/source/common/dataManager_6.java
+++ content/manual/source/common/dataManager_6.java
@@ -1,5 +1,6 @@
 BigDecimal sum = dataManager.loadValue(
         "select sum(o.amount) from demo$Order o " +
         "where o.date >= :date group by o.customer", BigDecimal.class)
+    .store("legacy_db") // <1>
     .parameter("date", orderDate)
     .one();
