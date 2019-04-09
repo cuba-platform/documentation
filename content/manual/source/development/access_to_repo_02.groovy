2e8a69699d01f0883ac237815a5d82b86d6e1a65
Mariya Sukhova <sukhova@haulmont.com>
2019-04-09 14:57:31 +0400

    Describe bintrayPremiumRepoUser Gradle property #419

--- content/manual/source/development/access_to_repo_02.groovy
+++ content/manual/source/development/access_to_repo_02.groovy
@@ -5,8 +5,8 @@ buildscript {
         maven {
             url 'https://cuba-platform.bintray.com/premium'
             credentials {
-                username(rootProject.hasProperty('premiumRepoUser') ?
-                        rootProject['premiumRepoUser'] : System.getenv('CUBA_PREMIUM_USER'))
+                username(rootProject.hasProperty('bintrayPremiumRepoUser') ?
+                        rootProject['bintrayPremiumRepoUser'] : System.getenv('CUBA_PREMIUM_USER'))
                 password(rootProject.hasProperty('premiumRepoPass') ?
                         rootProject['premiumRepoPass'] : System.getenv('CUBA_PREMIUM_PASSWORD'))
             }
