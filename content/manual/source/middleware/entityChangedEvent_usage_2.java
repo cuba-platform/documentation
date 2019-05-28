0790e32bff13bee5d2c026d67201171c2371d3bb
Konstantin Krivopustov <krivopustov@haulmont.com>
2019-05-28 13:34:58 +0400

    Example of changing related entities in EntityChangedEvent

---
+++ content/manual/source/middleware/entityChangedEvent_usage_2.java
@@ -0,0 +1,45 @@
+package com.company.sales.listener;
+
+import com.company.sales.entity.Order;
+import com.company.sales.entity.OrderLine;
+import com.haulmont.cuba.core.TransactionalDataManager;
+import com.haulmont.cuba.core.app.events.EntityChangedEvent;
+import com.haulmont.cuba.core.entity.contracts.Id;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import java.util.UUID;
+
+@Component("sales_OrderLineChangedListener")
+public class OrderLineChangedListener {
+
+    @Inject
+    private TransactionalDataManager txDm;
+
+    @EventListener
+    private void beforeCommit(EntityChangedEvent<OrderLine, UUID> event) {
+        Order order;
+        if (event.getType() != EntityChangedEvent.Type.DELETED) { // <1>
+            order = txDm.load(event.getEntityId()) // <2>
+                    .view("orderLine-with-order") // <3>
+                    .one()
+                    .getOrder(); // <4>
+        } else {
+            Id<Order, UUID> orderId = event.getChanges().getOldReferenceId("order"); // <5>
+            order = txDm.load(orderId).one();
+        }
+
+        long count = txDm.load(OrderLine.class) // <6>
+                .query("select o from sales_OrderLine o where o.order = :order")
+                .parameter("order", order)
+                .view("orderLine-with-product")
+                .list().stream()
+                .filter(orderLine -> Boolean.TRUE.equals(orderLine.getProduct().getSpecial()))
+                .count();
+
+        order.setNumberOfSpecialProducts((int) count);
+
+        txDm.save(order); // <7>
+    }
+}
