e52a76a68516404de2f66d2a8518767850a4f989
Konstantin Krivopustov <krivopustov@haulmont.com>
2019-05-30 14:17:20 +0400

    Fix Testing section

--- content/manual/source/development/testing_3.java
+++ content/manual/source/development/testing_3.java
@@ -1,8 +1,15 @@
+package com.company.sales;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SalesTestContainer extends TestContainer {
 
     public SalesTestContainer() {
         super();
-        appComponents = Arrays.asList(
+        appComponents = new ArrayList<>(Arrays.asList(
                 "com.haulmont.cuba"
                 // add CUBA premium add-ons here
                 // "com.haulmont.bpm",
@@ -10,7 +17,7 @@ public class SalesTestContainer extends TestContainer {
                 // "com.haulmont.fts",
                 // "com.haulmont.reports",
                 // and custom app components if any
-        );
+        ));
         appPropertiesFiles = Arrays.asList(
                 // List the files defined in your web.xml
                 // in appPropertiesConfig context parameter of the core module
@@ -18,11 +25,40 @@ public class SalesTestContainer extends TestContainer {
                 // Add this file which is located in CUBA and defines some properties
                 // specifically for test environment. You can replace it with your own
                 // or add another one in the end.
-                "test-app.properties",
-                "com/company/sales/test-app.properties");
+                "com/haulmont/cuba/testsupport/test-app.properties");
+        initDbProperties();
+    }
+
+    private void initDbProperties() {
         dbDriver = "org.postgresql.Driver";
         dbUrl = "jdbc:postgresql://localhost/sales_test";
         dbUser = "cuba";
         dbPassword = "cuba";
+    }
+
+    public static class Common extends SalesTestContainer {
+
+        // A common singleton instance of the test container which is initialized once for all tests
+        public static final SalesTestContainer.Common INSTANCE = new SalesTestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void before() throws Throwable {
+            if (!initialized) {
+                super.before();
+                initialized = true;
+            }
+            setupContext();
+        }
+
+        @Override
+        public void after() {
+            cleanupContext();
+            // never stops - do not call super
+        }
     }
 }
--- content/manual/source/development/testing_7.java
+++
@@ -1,7 +0,0 @@
-public class CustomerLoadTest {
-
-    @ClassRule
-    public static SalesTestContainer cont = SalesTestContainer.Common.INSTANCE;
-
-    ...
-}
