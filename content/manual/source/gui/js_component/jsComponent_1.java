905b5fc90471c7e24a2d99875e196980c27615e0
Konstantin Krivopustov <krivopustov@haulmont.com>
2019-07-04 09:34:38 +0400

    [JavaScriptComponent] Add more details regarding dependencies #488

--- content/manual/source/gui/js_component/jsComponent_1.java
+++ content/manual/source/gui/js_component/jsComponent_1.java
@@ -1,4 +1,7 @@
 jsComponent.addDependencies(
         "webjar://leaflet.js",
-        "webjar://leaflet-draw.js"
+        "http://code.jquery.com/jquery-3.4.1.min.js"
+);
+jsComponent.addDependency(
+        "http://api.map.baidu.com/getscript?v=2.0", DependencyType.JAVASCRIPT
 );
