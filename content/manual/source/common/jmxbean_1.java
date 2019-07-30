5ad003885c5c2603df3e158f8049341ec5982325
Mariya Sukhova <sukhova@haulmont.com>
2019-07-30 11:51:05 +0400

    Document JmxBean annotation #426

--- content/manual/source/common/jmxbean_1.java
+++ content/manual/source/common/jmxbean_1.java
@@ -1,10 +1,11 @@
 package com.sample.sales.core;
 
 import org.springframework.jmx.export.annotation.*;
+import com.haulmont.cuba.core.sys.jmx.JmxBean;
 
+@JmxBean(module = "sales", alias = "OrdersMBean")
 @ManagedResource(description = "Performs operations on Orders")
 public interface OrdersMBean {
-
     @ManagedOperation(description = "Recalculates an order amount")
     @ManagedOperationParameters({@ManagedOperationParameter(name = "orderId", description = "")})
     String calculateTotals(String orderId);
