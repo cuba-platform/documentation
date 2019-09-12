e95d54b0e93606a4d48d85becc1f24b873ac56c6
Konstantin Krivopustov <krivopustov@haulmont.com>
2019-09-12 12:09:30 +0400

    Remove Cookbook

--- content/manual/source/cookbook/window_params_4.java
+++
@@ -1,7 +0,0 @@
-openLookup("sample$Customer.browse",
-        items -> {
-            if (!items.isEmpty()) {
-                getItem().setCustomer((Customer) items.iterator().next());
-            }
-        },
-        WindowManager.OpenType.DIALOG.setWidth("600px").setHeight("400px"));
