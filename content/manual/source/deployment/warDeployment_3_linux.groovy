44112d3de42973d911f5c82559b3f7ed66990092
budarov <budarov@haulmont.com>
2019-11-20 19:21:51 +0400

    fix and update "5.3.5. WAR deployment to Tomcat Linux Service" section #501

--- content/manual/source/deployment/warDeployment_3_linux.groovy
+++ content/manual/source/deployment/warDeployment_3_linux.groovy
@@ -1,14 +1,11 @@
 task buildWar(type: CubaWarBuilding) {
-    appHome = '${catalina.base}/work'
-    singleWar = true
+    appHome = '${app.home}'
+    singleWar = false
     includeContextXml = true
     includeJdbcDriver = true
-    appProperties = ['cuba.automaticDatabaseUpdate': true]
-    webXmlPath = 'modules/web/web/WEB-INF/single-war-web.xml'
-    coreContextXmlPath = 'modules/core/web/META-INF/war-context.xml'
     appProperties = [
         'cuba.automaticDatabaseUpdate': true,
         'cuba.webPort': 9999,
-        'cuba.connectionUrlList': 'http://localhost:9999/app'
+        'cuba.connectionUrlList': 'http://localhost:9999/app-core'
     ]
 }
