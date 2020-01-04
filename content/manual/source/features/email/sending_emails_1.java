806ae96e273df1068e81ee0bd157292098e7637f
Mariya Sukhova <sukhova@haulmont.com>
2020-01-04 16:17:36 +0400

    Replace the example of using the legacy screen API for sending emails #625

--- content/manual/source/features/email/sending_emails_1.java
+++ content/manual/source/features/email/sending_emails_1.java
@@ -1,28 +1,36 @@
+@UiController("sample_NewsItem.edit")
+@UiDescriptor("news-item-edit.xml")
+@EditedEntityContainer("newsItemDc")
+@LoadDataBeforeShow
 public class NewsItemEdit extends StandardEditor<NewsItem> {
 
     private boolean justCreated; // <1>
 
     @Inject
     protected EmailService emailService;
-
+	
     @Inject
-    private Dialogs dialogs;
+    protected Dialogs dialogs;
 
     @Subscribe
     public void onInitEntity(InitEntityEvent<NewsItem> event) { // <2>
         justCreated = true;
     }
 
-    @Subscribe
-    public void onAfterCommitChanges(AfterCommitChangesEvent event) { // <3>
+    @Subscribe(target = Target.DATA_CONTEXT)
+    public void onPostCommit(DataContext.PostCommitEvent event) { // <3>
         if (justCreated) {
             dialogs.createOptionDialog() // <4>
                     .withCaption("Email")
                     .withMessage("Send the news item by email?")
+                    .withType(Dialogs.MessageType.CONFIRMATION)
                     .withActions(
-                            new DialogAction(DialogAction.Type.YES, Action.Status.PRIMARY).withHandler(e -> {
-                                sendByEmail();
-                            }),
+                            new DialogAction(DialogAction.Type.YES) {
+                                @Override
+                                public void actionPerform(Component component) {
+                                    sendByEmail();
+                                }
+                            },
                             new DialogAction(DialogAction.Type.NO)
                     )
                     .show();
@@ -31,12 +39,12 @@ public class NewsItemEdit extends StandardEditor<NewsItem> {
 
     private void sendByEmail() { // <5>
         NewsItem newsItem = getEditedEntity();
-
         EmailInfo emailInfo = EmailInfoBuilder.create()
-                .setAddresses("john.doe@company.com,jane.roe@company.com")
-                .setCaption(newsItem.getCaption())
-                .setTemplatePath("com/company/prj523/templates/news_item.txt")
-                .addTemplateParameter("newsItem", newsItem)
+                .setAddresses("john.doe@company.com,jane.roe@company.com") // <6>
+                .setCaption(newsItem.getCaption()) // <7>
+                .setFrom(null) // <8>
+                .setTemplatePath("com/company/demo/templates/news_item.txt") // <9>
+                .setTemplateParameters(Collections.singletonMap("newsItem", newsItem)) // <10>
                 .build();
         emailService.sendEmailAsync(emailInfo);
     }
