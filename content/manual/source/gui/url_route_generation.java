a5e1920552f2fe8a93d7490046109b19d346cbda
Mariya Sukhova <sukhova@haulmont.com>
2020-02-17 12:26:03 +0400

    Document URL Routes Generator #393 Some corrections were made

--- content/manual/source/gui/url_route_generation.java
+++ content/manual/source/gui/url_route_generation.java
@@ -7,16 +7,17 @@ private GroupTable<Customer> customersTable;
 @Inject
 private Dialogs dialogs;
 
-public void generateRoute() {
-   Customer selectedCustomer = customersTable.getSingleSelected();
-   if (selectedCustomer != null) {
-      String routeToSelectedRole = urlRouting.getRouteGenerator()
-         .getEditorRoute(selectedCustomer, ImmutableMap.of("someParam", "someValue"));
+@Subscribe("getLinkButton")
+public void onGetLinkButtonClick(Button.ClickEvent event) {
+    Customer selectedCustomer = customersTable.getSingleSelected();
+    if (selectedCustomer != null) {
+        String routeToSelectedRole = urlRouting.getRouteGenerator()
+                .getEditorRoute(selectedCustomer);
 
-         dialogs.createMessageDialog()
-                    .withCaption("Generated route")
-                    .withMessage(routeToSelectedRole)
-                    .withWidth("710")
-                    .show();
-      }
+        dialogs.createMessageDialog()
+                .withCaption("Generated route")
+                .withMessage(routeToSelectedRole)
+                .withWidth("710")
+                .show();
+    }
 }
