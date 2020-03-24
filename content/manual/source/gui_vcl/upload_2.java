771d30b890f861cefc89a7bd1f7419a972392745
Mariya Sukhova <sukhova@haulmont.com>
2020-03-24 14:36:07 +0400

    Update file upload example #645

--- content/manual/source/gui_vcl/upload_2.java
+++ content/manual/source/gui_vcl/upload_2.java
@@ -7,31 +7,30 @@ private DataManager dataManager;
 @Inject
 private Notifications notifications;
 
-@Subscribe
-protected void onInit(InitEvent event) {
-    uploadField.addFileUploadSucceedListener(uploadSucceedEvent -> {
-
-        File file = fileUploadingAPI.getFile(uploadField.getFileId()); <1>
-        if (file != null) {
-            notifications.create()
-                    .withCaption("File is uploaded to temporary storage at " + file.getAbsolutePath())
-                    .show();
-        }
-
-        FileDescriptor fd = uploadField.getFileDescriptor(); <2>
-        try {
-            fileUploadingAPI.putFileIntoStorage(uploadField.getFileId(), fd); <3>
-        } catch (FileStorageException e) {
-            throw new RuntimeException("Error saving file to FileStorage", e);
-        }
-        dataManager.commit(fd); <4>
+@Subscribe("uploadField")
+public void onUploadFieldFileUploadSucceed(FileUploadField.FileUploadSucceedEvent event) {
+    File file = fileUploadingAPI.getFile(uploadField.getFileId()); <1>
+    if (file != null) {
         notifications.create()
-                .withCaption("Uploaded file: " + uploadField.getFileName())
+                .withCaption("File is uploaded to temporary storage at " + file.getAbsolutePath())
                 .show();
-    });
+    }
 
-    uploadField.addFileUploadErrorListener(uploadErrorEvent ->
-            notifications.create()
-                    .withCaption("File upload error")
-                    .show());
+    FileDescriptor fd = uploadField.getFileDescriptor(); <2>
+    try {
+        fileUploadingAPI.putFileIntoStorage(uploadField.getFileId(), fd); <3>
+    } catch (FileStorageException e) {
+        throw new RuntimeException("Error saving file to FileStorage", e);
+    }
+    dataManager.commit(fd); <4>
+    notifications.create()
+            .withCaption("Uploaded file: " + uploadField.getFileName())
+            .show();
+}
+
+@Subscribe("uploadField")
+public void onUploadFieldFileUploadError(UploadField.FileUploadErrorEvent event) {
+    notifications.create()
+            .withCaption("File upload error")
+            .show();
 }
