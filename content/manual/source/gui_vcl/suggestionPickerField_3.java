941212f8b27b6b90162a1d4baed91cda36d2106c
Konstantin Krivopustov <krivopustov@haulmont.com>
2020-03-27 16:41:35 +0400

    Fix SuggestionPickerField example

--- content/manual/source/gui_vcl/suggestionPickerField_3.java
+++ content/manual/source/gui_vcl/suggestionPickerField_3.java
@@ -1,6 +1,6 @@
 suggestionPickerField.setSearchExecutor((searchString, searchParams) -> {
     searchString = QueryUtils.escapeForLike(searchString);
-    return dataManager.loadList(LoadContext.create(Customer.class).setQuery(
-            LoadContext.createQuery("select c from sample_Customer c where c.name like :name order by c.name escape '\\'")
-                .setParameter("name", "%" + searchString + "%")));
+    return dataManager.load(Customer.class)
+            .query("e.name like ?1 order by e.name escape '\\'", "%" + searchString + "%")
+            .list;
 });
