7b53d1f15587b415d00e6e321ffd2ba47d680106
Vlad Minaev <minaev@haulmont.com>
2020-03-28 14:32:17 +0400

    Minor code sample fix

--- content/manual/source/gui_vcl/suggestionPickerField_3.java
+++ content/manual/source/gui_vcl/suggestionPickerField_3.java
@@ -2,5 +2,5 @@ suggestionPickerField.setSearchExecutor((searchString, searchParams) -> {
     searchString = QueryUtils.escapeForLike(searchString);
     return dataManager.load(Customer.class)
             .query("e.name like ?1 order by e.name escape '\\'", "%" + searchString + "%")
-            .list;
+            .list();
 });
