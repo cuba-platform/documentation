ca95b563421421ad2902caf10d47a7facdc53549
Mariya Sukhova <sukhova@haulmont.com>
2020-04-01 17:59:56 +0400

    New features of the Tree component #602

--- content/manual/source/gui_vcl/treeExpandCollapseListener.java
+++ content/manual/source/gui_vcl/treeExpandCollapseListener.java
@@ -1,4 +1,11 @@
 @Subscribe("tree")
+public void onTreeExpand(Tree.ExpandEvent<Task> event) {
+    notifications.create()
+            .withCaption("Expanded: " + event.getExpandedItem().getName())
+            .show();
+}
+
+@Subscribe("tree")
 public void onTreeCollapse(Tree.CollapseEvent<Task> event) {
     notifications.create()
             .withCaption("Collapsed: " + event.getCollapsedItem().getName())
--- content/manual/source/gui_vcl/treeExpandListener.java
+++
@@ -1,6 +0,0 @@
-@Subscribe("tree")
-public void onTreeExpand(Tree.ExpandEvent<Task> event) {
-    notifications.create()
-            .withCaption("Expanded: " + event.getExpandedItem().getName())
-            .show();
-}
