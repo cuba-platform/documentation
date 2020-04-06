4392890256ecccabf4e8ebe317d3a4a365270a2f
Mariya Sukhova <sukhova@haulmont.com>
2020-04-06 17:45:45 +0400

    Fix the example for the Image component #683
    (cherry picked from commit fd1ce1b2a141f517c35f98107497e6d5d06d385f)

--- content/manual/source/gui_vcl/image_8.java
+++ content/manual/source/gui_vcl/image_8.java
@@ -1,6 +1,6 @@
 frameworksTable.addGeneratedColumn("image", entity -> {
     Image image = uiComponents.create(Image.NAME);
-    image.setDatasource(frameworksTable.getItemDatasource(entity), "image");
+    image.setValueSource(new ContainerValueSource<>(frameworksTable.getInstanceContainer(entity), "image"));
     image.setHeight("100px");
     return image;
 });
