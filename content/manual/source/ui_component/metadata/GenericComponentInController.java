5b43c63455f2becb3b6b063519e1f28b2701c05e
budarov <budarov@haulmont.com>
2020-09-10 17:39:34 +0400

    (merge) russian translation, some fixes, declaring sub-tag attributes, some improvements in en section #727

--- content/manual/source/ui_component/metadata/GenericComponentInController.java
+++ content/manual/source/ui_component/metadata/GenericComponentInController.java
@@ -2,7 +2,7 @@
 @UiDescriptor("dashboard.xml")
 public class Dashboard extends Screen {
     @Inject
-    private LazyTreeTable<Region> regionTable; // <1>
+    private MyTable<Region> regionTable; // <1>
 
     @Install(to = "regionTable", subject = "styleProvider")
     private String regionTableStyleProvider(Region region) { // <2>
