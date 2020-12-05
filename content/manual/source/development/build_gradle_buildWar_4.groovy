3af8efbd64511e1eda67682aa2cd7227d31cf0e7
Victor <zvictormail@gmail.com>
2020-12-05 17:18:50 +0300

    Update build_gradle_buildWar_4.groovy

--- content/manual/source/development/build_gradle_buildWar_4.groovy
+++ content/manual/source/development/build_gradle_buildWar_4.groovy
@@ -2,5 +2,5 @@ task buildWar(type: CubaWarBuilding) {
     singleWar = false
     // ...
     coreWebXmlPath = 'modules/core/web/WEB-INF/production-web.xml'
-    webWebXmlPath = 'modules/web/web/WEB-INF/production-web.xml'
+    webXmlPath = 'modules/web/web/WEB-INF/production-web.xml'
 }
