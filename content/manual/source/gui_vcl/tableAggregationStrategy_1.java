88f7eb465f4d5a3418115d99f44f82d1f33e1566
Mariya Sukhova <sukhova@haulmont.com>
2022-08-26 11:37:41 +0400

    Simplify AggregationStrategy sample #789

--- content/manual/source/gui_vcl/tableAggregationStrategy_1.java
+++ content/manual/source/gui_vcl/tableAggregationStrategy_1.java
@@ -1,14 +1,25 @@
-public class TimeEntryAggregation implements AggregationStrategy<List<TimeEntry>, String> {
+public class TableCustomerHobbyAggregation implements AggregationStrategy<Hobby, String> {
     @Override
-    public String aggregate(Collection<List<TimeEntry>> propertyValues) {
-        HoursAndMinutes total = new HoursAndMinutes();
-        for (List<TimeEntry> list : propertyValues) {
-            for (TimeEntry timeEntry : list) {
-                total.add(HoursAndMinutes.fromTimeEntry(timeEntry));
+    public String aggregate(Collection<Hobby> propertyValues) {
+        Hobby mostFrequent = null;
+        long max = 0;
+        if (CollectionUtils.isNotEmpty(propertyValues)) {
+            for (Hobby hobby : Hobby.values()) {
+                long current = propertyValues.stream()
+                        .filter(customerHobby -> customerHobby.equals(hobby))
+                        .count();
+                if (current > max) {
+                    mostFrequent = hobby;
+                    max = current;
+                }
             }
         }
-        return StringFormatHelper.getTotalDayAggregationString(total);
+        if (mostFrequent != null) {
+            return String.format("%s: %d/%d", mostFrequent.name(), max, propertyValues.size());
+        }
+        return null;
     }
+
     @Override
     public Class<String> getResultClass() {
         return String.class;
--- content/manual/source/gui_vcl/tableAggregationStrategy_2.java
+++ content/manual/source/gui_vcl/tableAggregationStrategy_2.java
@@ -1,6 +1,7 @@
+MetaPropertyPath metaPropertyPath = customersDc.getEntityMetaClass().getPropertyPath("hobby");
 AggregationInfo info = new AggregationInfo();
 info.setPropertyPath(metaPropertyPath);
-info.setStrategy(new TimeEntryAggregation());
+info.setStrategy(new TableCustomerHobbyAggregation());
 
-Table.Column column = weeklyReportsTable.getColumn(columnId);
+Table.Column column = customersTable.getColumn("hobby");
 column.setAggregation(info);
